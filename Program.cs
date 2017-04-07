59d2315dc524030d4d55dfde69ccfdbc8db5e0f1
Marius Moldovan <mariusmnd@hotmail.com>
2017-04-07 16:05:14 +0300

    added the FileInfoComparer in a separate source code file

--- Program.cs
+++ Program.cs
@@ -52,12 +52,4 @@ namespace LINQ
             }
         }
     }
-
-    public class FileInfoComparer : IComparer<FileInfo>
-    {
-        public int Compare(FileInfo x, FileInfo y)
-        {
-            return y.Length.CompareTo(x.Length);
-        }
-    }
 }
